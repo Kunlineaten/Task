963ffedd8e676f47d1ddfe6b2ce6d885d424413b
kunli <nancy5240105@gmail.com>
2025-09-21 17:11:37 +0800

    Update ssrjjcurrency.js

--- ssrjjcurrency.js
+++ ssrjjcurrency.js
@@ -10,7 +10,6 @@ const welfare_exchange_id = 200;
 const number = 8;
 
 if (!aSIGN || !UA || !APPDEVICE || !SMDEVICEID || !READERID || !IDENTIFICATION) {
-  $notification.post("❌ 缺少必要参数", "请先执行抓包脚本", "aSIGN, UA, APPDEVICE, SMDEVICEID, READERID, IDENTIFICATION");
   $done();
 }
 
