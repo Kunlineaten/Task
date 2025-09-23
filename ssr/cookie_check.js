675daf9e987b78dc45aa078539fa5d8e2a3621c3
kunli <nancy5240105@gmail.com>
2025-09-23 20:51:58 +0800

    Create cookie_check.js

---
+++ ssr/cookie_check.js
@@ -0,0 +1,19 @@
+// 读取已存储的参数
+const identification = $persistentStore.read("JJ_IDENTIFICATION");
+const ua = $persistentStore.read("JJ_UA");
+const appdevice = $persistentStore.read("JJ_APPDEVICE");
+const readerid = $persistentStore.read("JJ_READERID");
+const sign = $persistentStore.read("JJ_aSIGN");
+
+// 组织显示内容
+let message = `
+IDENTIFICATION: ${identification|| "未获取"}
+UA: ${ua|| "未获取"}
+APPDEVICE: ${appdevice|| "未获取"}
+READERID: ${readerid || "未获取"}
+aSIGN: ${sign || "未获取"}
+`;
+
+// 弹出通知
+$notification.post("📦 JJ 参数查看", "", message.trim());
+$done();
