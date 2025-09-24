49d28abd3b1d31c5c25d23ba0fb4ccd76ed0a73f
kunli <nancy5240105@gmail.com>
2025-09-24 16:01:54 +0800

    Update ssrjjcookie.js

--- ssr/ssrjjcookie.js
+++ ssr/ssrjjcookie.js
@@ -1,5 +1,6 @@
 /*
-* 原創於坤離，任何疑問參考：https://kunlineaten.notion.site
+ * 未经授权不得传播、修改或转售。脚本更新请以作者提供的最新版本为准。
+ * 若需获取更新或遇到问题，请通过原购买渠道联系作者，或访问网站：https://kunlineaten.notion.site
  */
 if ($request && $request.url.includes("executeExchange")) {
   const idMatch = $request.url.match(/[?&]identification=([^&]+)/);
